d3bada8258825fbf65581034f1a4a97a656e8ca0
unknown <grigorij.josifovski@gmail.com>
2025-04-11 17:38:57 +0200

    Add environment variables for specific datasets

--- src/config.py
+++ src/config.py
@@ -19,7 +19,12 @@ class Config:
     MINIO_CLIENT = Minio(MINIO_ENDPOINT_URL, access_key=MINIO_ACCESS_KEY, secret_key=MINIO_SECRET_KEY, secure=False)
 
     INPUT_DIRECTORY_PATH = Path(ENVIRONMENT_VARIABLES.get("INPUT_DIRECTORY_PATH"))
-    INPUT_FILE_NAME: Path = Path(ENVIRONMENT_VARIABLES.get("INPUT_FILE_NAME"))
+    STUDY_PROGRAMS_INPUT_DATA_FILE_NAME: Path = Path(ENVIRONMENT_VARIABLES.get("STUDY_PROGRAMS_INPUT_DATA_FILE_NAME"))
+    COURSES_INPUT_DATA_FILE_NAME: Path = Path(ENVIRONMENT_VARIABLES.get('COURSES_INPUT_DATA_FILE_NAME'))
+    PROFESSORS_INPUT_DATA_FILE_NAME: Path = Path(ENVIRONMENT_VARIABLES.get('PROFESSORS_INPUT_DATA_FILE_NAME'))
+    CURRICULA_INPUT_DATA_FILE_NAME: Path = Path(ENVIRONMENT_VARIABLES.get('CURRICULA_INPUT_DATA_FILE_NAME'))
+    TAUGHT_BY_INPUT_DATA_FILE_NAME: Path = Path(ENVIRONMENT_VARIABLES.get('TAUGHT_BY_INPUT_DATA_FILE_NAME'))
+    PREREQUISITES_INPUT_DATA_FILE_NAME: Path = Path(ENVIRONMENT_VARIABLES.get('PREREQUISITES_INPUT_DATA_FILE_NAME'))
 
     DATABASE_USER: str = ENVIRONMENT_VARIABLES.get("DATABASE_USER")
     DATABASE_PASSWORD: str = ENVIRONMENT_VARIABLES.get("DATABASE_PASSWORD")
@@ -37,19 +42,80 @@ class Config:
     DATABASE_RETRY_MULTIPLIER_IN_SECONDS: int = int(ENVIRONMENT_VARIABLES.get("DATABASE_RETRY_MULTIPLIER_IN_SECONDS"))
     DATABASE_RETRY_EXPONENT_BASE: int = int(ENVIRONMENT_VARIABLES.get("DATABASE_RETRY_EXPONENT_BASE"))
 
-    STUDY_PROGRAM_COLUMNS: list[str] = ["study_program_id",
-                                        "study_program_code",
-                                        "study_program_name",
-                                        "study_program_duration",
-                                        "study_program_url"]
-    COURSE_COLUMNS: list[str] = ["course_id", "course_code", "course_name_mk", "course_name_en", "course_url"]
-    PROFESSOR_COLUMNS: list[str] = ["course_professors_id",
-                                    "course_professors"]
-    CURRICULUM_COLUMNS: list[str] = ["study_program_id", "course_id", "course_level", "course_type", "course_semester",
-                                     "course_semester_season",
-                                     "course_academic_year"]
-    PREREQUISITE_COLUMNS: list[str] = ["course_id", "course_prerequisites_course_id",
-                                       "course_prerequisites_minimum_required_courses",
-                                       "course_prerequisites",
-                                       "course_prerequisite_type"]
-    TEACHES_COLUMNS: list[str] = ["course_id", "course_professors_id"]
+    STUDY_PROGRAM_COLUMNS: list[str] = [
+        "study_program_id",
+        "study_program_code",
+        "study_program_name",
+        "study_program_duration",
+        "study_program_url"
+    ]
+    COURSE_COLUMNS: list[str] = [
+        "course_id",
+        "course_code",
+        "course_name_mk",
+        "course_name_en",
+        "course_url"
+    ]
+    PROFESSOR_COLUMNS: list[str] = [
+        'professor_id',
+        'professor_name',
+        'professor_surname'
+    ]
+    CURRICULUM_COLUMNS: list[str] = [
+        'study_program_id',
+        'course_id',
+        'course_type',
+        'course_semester',
+        'course_semester_season',
+        'course_academic_year',
+        'course_level',
+    ]
+    PREREQUISITE_COLUMNS: list[str] = [
+        'course_id',
+        'course_prerequisite_type',
+        'course_prerequisite_id',
+        'minimum_required_number_of_courses'
+    ]
+    TEACHES_COLUMNS: list[str] = [
+        "course_id",
+        "professor_id"
+    ]
+
+    STUDY_PROGRAM_COLUMN_MAPPING: dict[str, str] = {
+        "study_program_id": "uid",
+        "study_program_code": "code",
+        "study_program_name": "name",
+        "study_program_duration": "duration",
+        "study_program_url": "url"
+    }
+    COURSE_COLUMN_MAPPING: dict[str, str] = {
+        "course_id": "uid",
+        "course_code": "code",
+        "course_name_mk": "name_mk",
+        "course_name_en": "name_en",
+        "course_url": "url"
+    }
+    PROFESSOR_COLUMN_MAPPING: dict[str, str] = {
+        "professor_id": "uid",
+        "professor_name": "name",
+        "professor_surname": "surname"
+    }
+    CURRICULUM_COLUMN_MAPPING: dict[str, str] = {
+        'study_program_id': 'study_program_id',
+        'course_id': 'course_id',
+        'course_level': 'level',
+        'course_type': 'type',
+        'course_semester': 'semester',
+        'course_semester_season': 'semester_season',
+        'course_academic_year': 'academic_year',
+    }
+    PREREQUISITE_COLUMN_MAPPING: dict[str, str] = {
+        'course_id': 'course_id',
+        'course_prerequisite_id': 'course_prerequisite_id',
+        'course_prerequisite_type': 'type',
+        'minimum_required_number_of_courses': 'number_of_courses'
+    }
+    TEACHES_COLUMN_MAPPING: dict[str, str] = {
+        "course_id": "course_id",
+        "professor_id": "professor_id"
+    }
